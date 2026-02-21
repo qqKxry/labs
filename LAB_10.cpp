ac1d32e7e88b67df5f9c3119ec638f3ccd69a01c
nightkory <136008955+qqKxry@users.noreply.github.com>
2026-02-22 00:54:22 +0300

    Add files via upload

--- LAB_10.cpp
+++ LAB_10.cpp
@@ -3,32 +3,10 @@
 #include <locale>
 #include <string>
 #include <cmath>
+#include <algorithm>
+#include <cstdlib>
 
 using namespace std;
-void Task1(vector<vector<int>>& Matrix) {
-    vector<int> columnSums(Matrix[0].size(), 0);
-    int totalSum = 0;
-    for (int i = 0; i < Matrix.size(); i++) {
-
-        int RowSum = 0;
-
-        for (int j = 0; j < Matrix[0].size(); j++) {
-            RowSum += Matrix[i][j];
-            columnSums[j] += Matrix[i][j];
-        }
-
-        Matrix[i].push_back(RowSum);
-        totalSum += RowSum;
-    }
-    columnSums.push_back(totalSum);
-
-    Matrix.push_back(columnSums);
-}
-
-void Task2(vector<vector<int>>& Matrix) {
-    int order;
-    cout << "Выберите порядок:" << endl << "1. По возрастани." << endl << "2. По убыванию" << endl;
-}
 
 int FindLongest(const vector<vector<int>>& Matrix) {
     int MaxSize = 1;
@@ -89,6 +67,266 @@ void DrawTable(int MatrixRows, int MatrixColumns, vector<vector<int>>& Matrix) {
     cout << "┘\n";
 }
 
+void Task1(vector<vector<int>>& Matrix) {
+    vector<int> columnSums(Matrix[0].size(), 0);
+    int totalSum = 0;
+    for (int i = 0; i < Matrix.size(); i++) {
+
+        int RowSum = 0;
+
+        for (int j = 0; j < Matrix[0].size(); j++) {
+            RowSum += Matrix[i][j];
+            columnSums[j] += Matrix[i][j];
+        }
+
+        Matrix[i].push_back(RowSum);
+        totalSum += RowSum;
+    }
+    columnSums.push_back(totalSum);
+
+    Matrix.push_back(columnSums);
+
+    DrawTable(Matrix.size()+1, Matrix[0].size()+1, Matrix);
+}
+
+void Task2(vector<vector<int>>& Matrix) {
+    int MatrixRows = Matrix.size();
+    int MatrixColumns = Matrix[0].size();
+    int order;
+    cout << "Выберите порядок:" << endl << "1. По возрастани." << endl << "2. По убыванию" << endl;
+
+    cin >> order;
+
+    if (order == 1) {
+        for (int i = 0; i < MatrixRows; i++) {
+            for (int j = 0; j < Matrix[i].size(); j++) {
+                sort(Matrix[i].begin(), Matrix[i].end());
+            }
+        }
+        DrawTable(MatrixRows, MatrixColumns, Matrix);
+    } else if ( order == 2 ) {
+        for (int i = 0; i < MatrixRows; i++) {
+            for (int j = 0; j < Matrix[i].size(); j++) {
+                sort(Matrix[i].begin(), Matrix[i].end());
+
+                reverse(Matrix[i].begin(), Matrix[i].end());
+            }
+        }
+        DrawTable(MatrixRows, MatrixColumns, Matrix);
+    } else {
+        cout << "Такой опции не существует." << endl;
+    }
+}
+
+void Task3(vector<vector<int>>& Matrix) {
+    int MatrixRows = Matrix.size();
+    int MatrixColumns = Matrix[0].size();
+    int order;
+    vector<int> tArray;
+    cout << "Выберите порядок:" << endl << "1. По возрастани." << endl << "2. По убыванию" << endl;
+
+    cin >> order;
+
+    if (order == 1) {
+        for (int i = 0; i < MatrixColumns; i++) {
+            for (auto&x : Matrix) {
+                tArray.push_back(x[i]); 
+            }
+
+            sort(tArray.begin(), tArray.end());
+
+            for (int j = 0; j < MatrixColumns; j++) {
+                Matrix[j][i] = tArray[j];
+            }
+            
+            tArray.clear();
+        }
+
+        DrawTable(MatrixRows, MatrixColumns, Matrix);
+
+    } else if ( order == 2 ) {
+        for (int i = 0; i < MatrixColumns; i++) {
+            for (auto&x : Matrix) {
+                tArray.push_back(x[i]); 
+            }
+
+            for (auto x : tArray) cout << x << ' ';
+            cout << endl;
+
+            sort(tArray.begin(), tArray.end());
+            reverse(tArray.begin(), tArray.end());
+            
+            for (int j = 0; j < MatrixColumns; j++) {
+                Matrix[j][i] = tArray[j];
+            }
+            
+            tArray.clear();
+        }
+
+        DrawTable(MatrixRows, MatrixColumns, Matrix);
+
+    } else {
+        cout << "Такой опции не существует." << endl;
+    }
+}
+
+int Task4(vector<vector<int>>& Matrix) {
+    int MatrixRows = Matrix.size();
+    int MatrixColumns = Matrix[0].size();
+    int reviewed, place;
+    cout << "Выберите обозреваемую строку:" << endl;
+    cin >> reviewed;
+
+    if (reviewed > MatrixRows) {
+        cout << "Такой строки не существует." << endl;
+        return 0;
+    } if (reviewed < 0) {
+        cout << "Номер строки не может быть отрицательным или равным нулю." << endl;
+        return 0;
+    }
+
+    cout << "Выберите строку для перестановки:" << endl;
+    cin >> place;
+
+    if (place > MatrixRows) {
+        cout << "Такой строки не существует." << endl;
+        return 0;
+    } if (place < 0) {
+        cout << "Номер строки не может быть отрицательным или равным нулю." << endl;
+        return 0;
+    }
+
+    Matrix.at(reviewed - 1).swap(Matrix.at(place - 1));
+    DrawTable(MatrixRows, MatrixColumns, Matrix);
+
+    return 0;
+}
+
+int Task5(vector<vector<int>>& Matrix) {
+    int MatrixRows = Matrix.size();
+    int MatrixColumns = Matrix[0].size();
+
+    vector<int> tArray1, tArray2;
+    int reviewed, place;
+    cout << "Выберите обозреваемый столбец:" << endl;
+    cin >> reviewed;
+
+    if (reviewed > MatrixRows) {
+        cout << "Такого столбца не существует." << endl;
+        return 0;
+    } if (reviewed < 0) {
+        cout << "Номер столбца не может быть отрицательным или равным нулю." << endl;
+        return 0;
+    }
+
+    cout << "Выберите столбец для перестановки:" << endl;
+    cin >> place;
+
+    if (place > MatrixRows) {
+        cout << "Такого столбца не существует." << endl;
+        return 0;
+    } if (place < 0) {
+        cout << "Номер столбца не может быть отрицательным или равным нулю." << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < MatrixRows; i++) {
+        tArray1.push_back(Matrix[i][reviewed-1]);
+        tArray2.push_back(Matrix[i][place-1]);
+    }
+
+    for (int i = 0; i < MatrixRows; i++) {
+        Matrix[i][reviewed-1] = tArray2[i];
+        Matrix[i][place-1] = tArray1[i];
+    }
+
+    DrawTable(MatrixRows, MatrixColumns, Matrix);
+
+    return 0;
+}
+
+void Task6(vector<vector<int>>& Matrix) {
+    int MatrixRows = Matrix.size();
+    int MatrixColumns = Matrix[0].size();
+    vector<int> Minimums;
+
+    for (auto& Row : Matrix) {
+        sort(Row.begin(), Row.end());
+        Minimums.push_back(Row[0]);
+    }
+
+    for (int i = 0; i < Minimums.size(); i++) {
+        cout << "Минимальный элемент для " << i + 1 << " строки: " << Minimums[i] << endl;
+    }
+}
+
+void Task7(vector<vector<int>>& Matrix) {
+    int MatrixRows = Matrix.size();
+    int MatrixColumns = Matrix[0].size();
+    vector<int> Maximus;
+
+    for (int i = 0; i < MatrixRows; i++) {
+        for (int j = 0; j < MatrixColumns; j++) Maximus.push_back(Matrix[i][j]);
+
+        sort(Maximus.begin(), Maximus.end());
+
+        cout << "Максимальный элемент для " << i + 1 << " столбца: " << Maximus[MatrixColumns - 1] << endl;
+
+        Maximus.clear();
+    }
+}
+
+void Task8(vector<vector<int>>& Matrix) {
+    int MatrixRows = Matrix.size();
+    int MatrixColumns = Matrix[0].size();
+    int Diagonal = 0;
+
+    for (int s = 0; s < MatrixColumns; s++) {
+        for (int a = 0; a < MatrixColumns; a++) {
+            for (int b = 0; b < MatrixRows; b++) {
+                if (a == b) { 
+                    Diagonal += Matrix[b][a]; 
+                }
+            }
+        }
+    }
+    cout << "Сумма элементов главного диагонали = " << Diagonal << endl;
+}
+
+void Task9(vector<vector<int>>& Matrix) {
+    int MatrixRows = Matrix.size();
+    int MatrixColumns = Matrix[0].size();
+    int Diagonal = 0;
+
+    for (int a = 0; a < MatrixColumns; a++) {
+        for (int b = 0; b < MatrixRows; b++) {
+            if (a + b == MatrixColumns - 1) { 
+                Diagonal += Matrix[b][a]; 
+            }
+        }
+    }
+
+    cout << "Сумма элементов побочной диагонали = " << Diagonal << endl;
+}
+
+void TaskX(vector<vector<int>>& Matrix) {
+    int MatrixRows = Matrix.size();
+    int MatrixColumns = Matrix[0].size();
+
+    vector<vector<int>> NewMatrix(MatrixRows, vector<int>(MatrixColumns, 0));
+    vector<int> tArray;
+
+    for (int i = 0; i < MatrixRows; i++) {
+        for (int j = 0; j < MatrixColumns; j++) {
+            tArray.push_back(Matrix[j][i]);
+        }
+        NewMatrix[i] = tArray;
+        tArray.clear();
+    }
+
+    DrawTable(MatrixRows, MatrixColumns, NewMatrix);
+}
+
 int main() {
     setlocale(LC_ALL, ".UTF8");
     int MatrixRows, MatrixColumns;
@@ -109,7 +347,7 @@ int main() {
         for (int x = 0; x < MatrixColumns; x++) {
             cout << "\nEnter x" << x+1 << " y" << y+1 << ": ";
             cin >> Matrix[y][x];
-            cout << "\033[H\033[J";
+            system("cls");
             DrawTable(MatrixRows, MatrixColumns, Matrix);
         }
     }
@@ -131,11 +369,16 @@ int main() {
 
     switch (q)
     {
-    case 1:
-        Task1(Matrix);
-        DrawTable(MatrixRows+1, MatrixColumns+1, Matrix);
-        break;
-    
+    case 1: Task1(Matrix); break;
+    case 2: Task2(Matrix); break;
+    case 3: Task3(Matrix); break;
+    case 4: Task4(Matrix); break;
+    case 5: Task5(Matrix); break;
+    case 6: Task6(Matrix); break;
+    case 7: Task7(Matrix); break;
+    case 8: Task8(Matrix); break;
+    case 9: Task9(Matrix); break;
+    case 10: TaskX(Matrix); break;
     default:
         break;
     }
